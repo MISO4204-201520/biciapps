a51b36a49b9177c3d961e5d915d43392141158c1
Omar <omarcz27@gmail.com>
2015-10-18 07:56:17 -0500

    Ajustes Modulo Seguridad - Formulario de Inicio de Sesión y de Registrar Usuario

--- public/javascripts/inicioSesion.js
+++ public/javascripts/inicioSesion.js
@@ -1,6 +1,18 @@
 /**
  * Created by Omar on 14/10/2015.
  */
+$( document ).ready(function() {
+    console.log( "ready!" );
+    var sexoOculto = $("#sexoOculto").val();
+    if(sexoOculto.toLowerCase() === "Masculino".toLowerCase()) {
+        $("#masc").prop("checked", true)
+    }
+    else {
+        $("#feme").prop("checked", true)
+    }
+});
+
+
 function validarContrasena(){
     var pass = $("#pwd").val();
     var confirmarPass = $("#pwd1").val();
