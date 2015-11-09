678fd4a8e4bb51fe307fda6107846d760e1407fd
Jhon Fredy Gutierrez Quiceno <jf.gutierrez20@uniandes.edu.co>
2015-11-08 23:17:36 -0500

    Se incluyen constantes de Configurador de Bicicletas

---
+++ app/constantes/ConstantesConfBicicleta.java
@@ -0,0 +1,20 @@
+package constantes;
+
+public class ConstantesConfBicicleta {
+	public static final String MONTANA = "montana";
+	public static final String RUTA = "ruta";
+	public static final String CROSS = "cross";
+	
+	public static final String RIGIDO = "rigido";
+	public static final String SUSP = "susp";
+
+	public static final String TENEDOR_RIGIDO = "tenedorRigido";
+	public static final String TENEDOR_SUSPENSION = "tenedorSuspension";
+	
+	public static final String GRABADO = "grabado";
+	public static final String PISTERA = "pistera";
+	
+	public static final String DISCO1 = "1disco";
+	public static final String DISCO2 = "2disco";
+	public static final String DISCO3 = "3disco";	
+}
