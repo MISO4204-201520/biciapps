96cf1e9d3e4d27fdb975133d563af529a7138808
luis f <le.florez602@uniandes.edu.co>
2015-11-21 09:04:38 -0500

    Primer intento del Script para las variables de entorno

---
+++ confHeroku.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import os
+
+#Variables de feature ide con su variable de entorno correspondiente
+featureVars = { 
+        'Sitios':  'SITIOS',
+        'Alquileres':  'ALQUILERES',
+        'Individual':  'INDIVIDUAL',
+        'Grupal':  'GRUPAL',
+        'Metricas':    'METRICAS',
+        'HistorialViajes': 'HISTORIALVIAJES',
+        'ManejoPerfiles':  'MANEJOPERFILES',
+        'RedesSociales':   'REDESSOCIALES',
+        'Twitter': 'TWITTER',
+        'Facebook':    'FACEBOOK',
+        'CompartirRedesSociales':  'COMPARTIRREDESSOCIALES',
+        'GestionAmigos':   'GESTIONAMIGOS',
+        'Notificaciones':  'NOTIFICACIONES',
+        'Reportes':    'REPORTES',
+        'ReporteMetricas': 'REPORTEMETRICAS',
+        'ReporteRutas':    'REPORTERUTAS',
+        'ReporteHistorialViajes':  'REPORTEHISTORIALVIAJES',
+        'ConfiguradorBicicletas':  'CONFIGURADORBICICLETAS',
+        'PromocionesContexto': 'PROMOCIONESCONTEXTO'
+    }
+
+
+enabledfeatures = {key: False for key in featureVars.keys()}   
+
+infile = open("features.txt", "r") 
+
+for line in infile:
+    if line.strip() in enabledfeatures:
+        enabledfeatures[line.strip()] = True
+
+
+
+#print enabledfeatures
+
+#Imprimir las variables puestas
+#os.system('heroku config')
+
+for  k, v in enabledfeatures.iteritems():
+    if v == True:
+        #print "heroku config:set "+featureVars[k]+"=TRUE"
+        os.system("heroku config:set "+featureVars[k]+"=TRUE")
+    else:
+        #print "heroku config:set "+featureVars[k]+"=FALSE"
+        os.system("heroku config:set "+featureVars[k]+"=FALSE")
+
+#os.system("heroku config:set REPORTEMETRICAS=TRUE")
+#os.system('heroku config:set REPORTEMETRICAS=FALSE')
